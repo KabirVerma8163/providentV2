719c5ce55c345ebb169120cf5147e50ba81ab9c8
Dongtai <dongtaishi.w@gmail.com>
2021-08-21 22:47:19 -0400

    stocks fix

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -186,25 +186,6 @@ function createCandlestickGraph(symbol, type, timeRange, elementName)
     });
   });
 }
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
 function searchSymbol()
 {
   let symbol = document.getElementById("stockInput").value;
